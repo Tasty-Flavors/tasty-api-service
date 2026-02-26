fa0192935a1e79e777ce660f8ab7e0764de92df1
FelipeLafin <felahau@gmail.com>
2026-02-26 13:29:05 -0300

    separando back e adicionando arquivos docker

---
+++ src/main/java/app/tasty_flavors/TastyFlavorsBackEnd.java
@@ -0,0 +1,13 @@
+package app.tasty_flavors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyFlavorsBackEnd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyFlavorsBackEnd.class, args);
+    }
+
+}
